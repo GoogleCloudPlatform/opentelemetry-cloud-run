134ed146d5ccae63567edfca37d62ba5bec4f827
Pranav Sharma <sharmapranav@google.com>
2025-06-23 23:53:28 +0000

    Add dotnet sample

---
+++ csharp/helloworld-csharp/Program.cs
@@ -0,0 +1,38 @@
+// Sample adopted from https://opentelemetry.io/docs/languages/dotnet/getting-started
+
+using System.Globalization;
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var url = $"http://0.0.0.0:{port}";
+
+var app = builder.Build();
+
+var logger = app.Logger;
+
+int RollDice()
+{
+    return Random.Shared.Next(1, 7);
+}
+
+string HandleRollDice(string? player)
+{
+    var result = RollDice();
+
+    if (string.IsNullOrEmpty(player))
+    {
+        logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
+    }
+    else
+    {
+        logger.LogInformation("{player} is rolling the dice: {result}", player, result);
+    }
+
+    return result.ToString(CultureInfo.InvariantCulture);
+}
+
+app.MapGet("/rolldice/{player?}", HandleRollDice);
+
+app.Run(url);
